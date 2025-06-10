fbfa5b60ecbe3a82f4bbb0d38460c715a1ee8f0b
Djudo71 <djudo71@meta.ua>
2025-06-10 16:17:58 +0300

    Corrected

--- app/main.py
+++ app/main.py
@@ -10,27 +10,22 @@ class Person:
 def create_person_list(people_dicts: list[dict]) -> list[Person]:
     Person.people = {}
 
-    person_instances = []
-
-    for person_dict in people_dicts:
-        name = person_dict["name"]
-        age = person_dict["age"]
-        person_instances.append(Person(name, age))
+    person_instances = [Person(p["name"], p["age"]) for p in people_dicts]
 
     for person_dict in people_dicts:
         current_person_name = person_dict["name"]
         current_person_obj = Person.people[current_person_name]
 
-        if "wife" in person_dict and person_dict["wife"] is not None:
-            wife_name = person_dict["wife"]
+        wife_name = person_dict.get("wife")
+        if wife_name:
             if wife_name in Person.people:
                 current_person_obj.wife = Person.people[wife_name]
             else:
                 print(f"Warning: Wife \"{wife_name}\" not found for "
                       f"\"{current_person_obj.name}\".")
 
-        elif "husband" in person_dict and person_dict["husband"] is not None:
-            husband_name = person_dict["husband"]
+        husband_name = person_dict.get("husband")
+        if husband_name:
             if husband_name in Person.people:
                 current_person_obj.husband = Person.people[husband_name]
             else:
